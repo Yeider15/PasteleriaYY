d4db9407665aaafb09048e5b840c0f30446c2614
Yeider15 <ye.catari@duocuc.cl>
2025-11-05 16:44:18 -0300

    Se crean model Producto y Promocion

---
+++ app/src/main/java/com/example/pasteleriayy/model/Promocion.kt
@@ -0,0 +1,9 @@
+package com.example.pasteleriayy.model
+
+data class Promocion(
+    val id: Int,
+    val nombre: String,
+    val descripcion: String,
+    val fechaFin: String,
+    val precio: Double
+)
