3eb768225dee6b3b59bd3865ccd890343af1d8fc
yaquelin2305 <ya.rugel@duocuc.cl>
2025-11-05 20:45:06 -0300

    actualizando

--- app/src/main/java/com/example/pasteleriayy/model/Usuario.kt
+++ app/src/main/java/com/example/pasteleriayy/model/Usuario.kt
@@ -1,6 +1,6 @@
 package com.example.pasteleriayy.model
 data class Usuario (
-    val apellido: String,
+    val nombre: String,
     val correo: String,
     val contrasena: String,
     val telefono: String? = null
