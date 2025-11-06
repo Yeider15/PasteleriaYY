97845735cfc28141d2f0b29cd84891d02595d55f
Yeider15 <ye.catari@duocuc.cl>
2025-11-05 21:31:57 -0300

    Se termina de agregar productos y imagenes

--- app/src/main/java/com/example/pasteleriayy/data/ProductosDataSource.kt
+++ app/src/main/java/com/example/pasteleriayy/data/ProductosDataSource.kt
@@ -16,6 +16,26 @@ object ProductosDataSource {
 
         Producto("TT001", "Torta Circular de Vainilla",
             "Bizcocho de vainilla clásico relleno con crema...",
-            40000.0, R.drawable.tt001)
+            40000.0, R.drawable.tt001),
+
+        Producto("TT002", "Torta Circular de Manjar",
+            "Torta tradicional chilena con manjar y nueces...",
+            42000.0, R.drawable.tt002),
+
+        Producto("P1001", "Mousse de Chocolate",
+            "Postre individual cremoso y suave...",
+            5000.0, R.drawable.p1001),
+
+        Producto("P1002", "Tiramisú Clásico",
+            "Un postre italiano individual con capas de café, mascarpone y cacao...",
+            5500.0, R.drawable.p1002),
+
+        Producto("PSA001", "Torta Sin Azúcar de Naranja",
+            "Torta ligera y deliciosa, endulzada naturalmente...",
+            48000.0, R.drawable.psa001),
+
+        Producto("PSA002", "Cheesecake Sin Azúcar",
+            " Suave y cremoso...",
+            47000.0, R.drawable.psa002)
     )
 }
